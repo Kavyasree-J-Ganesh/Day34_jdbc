9739bde39057ae6bd3acadd2aa4561c0e5246aa9
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2023-01-26 18:06:25 +0530

    [Kavyasree J Ganesh] Jdbc employee payroll

--- src/main/java/bridgelabz/EmpPayRollDBService.java
+++ src/main/java/bridgelabz/EmpPayRollDBService.java
@@ -21,5 +21,28 @@ public class EmpPayRollDBService {
         return connection;
     }
 
+    /**
+     * retreiving the data from the database to get person salary
+     */
+    public List<EmpPayRollData> readData() {
+        String sql = "select * from employee_payroll ";
+        List<EmpPayRollData> employeePayRollList = new ArrayList<>();
+        try {
+            Connection connection = this.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                double basic_pay = result.getDouble("basic_pay");
+                LocalDate startDate = result.getDate("start_date").toLocalDate();
+                employeePayRollList.add(new EmpPayRollData(id, name, basic_pay, startDate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayRollList;
+    }
+
 
 }
--- src/main/java/bridgelabz/JDBCMain.java
+++ src/main/java/bridgelabz/JDBCMain.java
@@ -2,6 +2,7 @@ package bridgelabz;
 
 public class JDBCMain {
     public static void main(String[] args) {
-
+       EmpPayRollDBService service = new EmpPayRollDBService();
+       System.out.println(service.readData());
     }
 }
